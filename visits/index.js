83edf874cd69c73f5f02fcacd1afc33f8a6c445b
Mano Sriram <mano.sriram0@gmail.com>
2021-08-17 08:19:59 +0530

    Update index

--- visits/index.js
+++ visits/index.js
@@ -3,18 +3,12 @@ const app = express()
 const process = require('process')
 const PORT = process.env.PORT || 5050
 
-const redis = require("redis")
-const client = redis.createClient({
-    host: 'redis-server' // 'redis' service name in ./docker-compose.yml
-})
-client.set("visits", 0)
-
+var counter = 0;
 app.get("/", (req, res) => {
-    client.get("visits", (err, visits) => {
-        const result = `Number of visits = ${visits}`;
-        res.send(result)
-        client.set("visits", parseInt(visits)+1)
-    });
+    const send = `Hits = ${counter}`
+    counter++
+
+    res.send(send)
 });
 
 app.listen(PORT, () => {
